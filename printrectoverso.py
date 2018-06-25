f5c787616c9c57d1effedd7edc47cd0373e85493
Jerome Belleman <jerome.belleman@gmail.com>
2018-06-25 10:30:42 +0200

    Join PDFs, ready to be printed duplex

---
+++ printrectoverso.py
@@ -0,0 +1,33 @@
+'''
+Join PDFs and open the result in Evince, ready to be printed duplex
+'''
+
+import subprocess
+from gi.repository import Nautilus, GObject
+
+
+def printrectoverso(_, files):
+    '''
+    Run pdfjoin, then Evince
+    '''
+
+    subprocess.call(['pdfjoin'] + \
+                    [fle.get_uri()[7:] for fle in files] + \
+                    ['-o', '/tmp/printrectoverso.pdf'])
+    subprocess.Popen(['evince', '/tmp/printrectoverso.pdf'])
+
+
+class PrintRectoVerso(GObject.GObject, Nautilus.MenuProvider):
+    '''
+    Menu provider
+    '''
+
+    def get_file_items(self, _, files): # pylint: disable=arguments-differ
+        for fle in files:
+            if fle.get_mime_type() != 'application/pdf':
+                return
+
+        item = Nautilus.MenuItem(name='Nautilus::printrectoverso',
+                                 label='Print Recto/Verso')
+        item.connect('activate', printrectoverso, files)
+        return item,
